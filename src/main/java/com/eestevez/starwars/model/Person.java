512612238e3bd3cf934928dc7812c4b7ae8c74b9
Enrique <estevezp81@gmail.com>
2025-08-04 11:57:49 +0200

    Display formatted dates in view

--- src/main/java/com/eestevez/starwars/model/Person.java
+++ src/main/java/com/eestevez/starwars/model/Person.java
@@ -3,6 +3,7 @@ package com.eestevez.starwars.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 public record Person(
@@ -18,4 +19,11 @@ public record Person(
         OffsetDateTime created,
         OffsetDateTime edited
 ) {
+    public String getCreatedFormatted() {
+        return created != null ? created.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) : "";
+    }
+
+    public String getEditedFormatted() {
+        return edited != null ? edited.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) : "";
+    }
 }
--- src/main/java/com/eestevez/starwars/model/Starship.java
+++ src/main/java/com/eestevez/starwars/model/Starship.java
@@ -3,6 +3,7 @@ package com.eestevez.starwars.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 
 public record Starship (
         String name,
@@ -14,4 +15,12 @@ public record Starship (
         @JsonProperty("starship_class") String type,
         OffsetDateTime created,
         OffsetDateTime edited) {
+    public String getCreatedFormatted() {
+        return created != null ? created.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) : "";
+    }
+
+    public String getEditedFormatted() {
+        return edited != null ? edited.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) : "";
+    }
+
 }
